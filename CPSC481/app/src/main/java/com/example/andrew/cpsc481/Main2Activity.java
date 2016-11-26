5e924204e92dc91fc3800836c7c2f4bed792adb8
Andrew <pext99@gmail.com>
2016-11-25 17:00:37 -0700

    when item on list gets clicked, goes to new activity

--- CPSC481/app/src/main/java/com/example/andrew/cpsc481/Main2Activity.java
+++ CPSC481/app/src/main/java/com/example/andrew/cpsc481/Main2Activity.java
@@ -1,37 +1,21 @@
 package com.example.andrew.cpsc481;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-//https://www.youtube.com/watch?v=UaIDyMebJSw (search reference)
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
-    private ListView lstSearch;
-    private EditText edtSearch;
-    private ArrayAdapter<String> adapter;
-
-    String data[] = {
-            "banana", "apple", "orange", "grapefruit",
-            "celery", "spinach", "tomato", "lettuce", "squash",
-            "chips", "pepsi", "sprite",
-            "ham", "bacon", "salmon", "tuna",
-            "swiss cheese", "cheddar"
-    };
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -41,47 +25,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-        lstSearch = (ListView) findViewById(R.id.lstSearch);
-        edtSearch = (EditText) findViewById(R.id.edtSearch);
-        adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.textView, data);
-        lstSearch.setAdapter(adapter);
-        
-        lstSearch.setOnItemClickListener(
-                new AdapterView.OnItemClickListener(){
+        String[] chips = {"Lay's - Original", "Pringles - Sour Cream & Onion", "Doritos - Cool Ranch"};
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, chips);
+        ListView chipsListView = (ListView)  findViewById(R.id.listOfChips);
+        chipsListView.setAdapter(adapter);
 
+        /*
+        chipsListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        String data = String.valueOf(parent.getItemAtPosition(position));
-                        Toast.makeText(MainActivity.this, data, Toast.LENGTH_LONG).show();
-                        startActivity(new Intent(getApplicationContext(), Main2Activity.class)); //goes to new activity
+                        String chips = String.valueOf(parent.getItemAtPosition(position));
+
                     }
                 }
-        );
-
-
-        edtSearch.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                MainActivity.this.adapter.getFilter().filter(s);
-                if (s.length() > 0) {
-                    lstSearch.setVisibility(View.VISIBLE);
-                } else {
-                    lstSearch.setVisibility(View.GONE);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
+        );*/
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
@@ -93,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public Action getIndexApiAction() {
         Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
+                .setName("Main2 Page") // TODO: Define a title for the content shown.
                 // TODO: Make sure this auto-generated URL is correct.
                 .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
                 .build();
